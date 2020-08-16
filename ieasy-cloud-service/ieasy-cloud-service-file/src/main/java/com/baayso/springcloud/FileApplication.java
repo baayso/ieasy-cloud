c0b6b9d01a22c89d0867f636b744232a0d2398c9
baayso <baayso@outlook.com>
2020-08-16 22:03:36 +0800

    refactor(pom.xml): 整理依赖。

--- ieasy-cloud-service/ieasy-cloud-service-file/src/main/java/com/baayso/springcloud/FileApplication.java
+++ ieasy-cloud-service/ieasy-cloud-service-file/src/main/java/com/baayso/springcloud/FileApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-
 /**
  * 程序启动类。
  *
@@ -13,7 +11,7 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
  * @since 0.1
  */
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class) // 排除数据库自动加载，此项目不需要使用数据库
+@SpringBootApplication
 public class FileApplication {
 
     public static void main(String[] args) {
